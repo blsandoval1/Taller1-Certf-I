7375b4f5f017812183362f8efc4b0c067528fc8d
blsandoval1 <blsandoval1@espe.edu.ec>
2020-06-11 11:27:32 -0500

    Trabajo en clase ASP.NET

---
+++ SlnTaller1/BEEUTaller1/Transactions/PeliculaBLL.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEEUTaller1.Transactions
+{
+    public class PeliculaBLL
+    {
+        public static void Create(Pelicula p)
+        {
+            //Empezamos la transaccion
+            //Siempre que yo CREAR, ACTUALIZA, ELIMINAR debo utilizar una transaccion
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Peliculas.Add(p);
+                        db.SaveChanges();
+                        transaction.Commit();//Comprometer cambios
+                    }
+                    catch (Exception ex)
+                    {
+                        //Rollback -> para evitar datos inconsistentes
+                        transaction.Rollback();//Todo se elimina
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        //Para que el alumno este dentro del mismo contexto se hace lo siguiente: (PARA UPDATE)
+        //GET-CONSULTAR
+        public static Pelicula Get(int? id)
+        {
+            Entities db = new Entities();
+            return db.Peliculas.Find(id);
+        }
+
+        public static void Update(Pelicula pelicula)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Peliculas.Attach(pelicula);
+                        db.Entry(pelicula).State = System.Data.Entity.EntityState.Modified; //Attach en lugar de SAaveChanges es otra opcion
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Pelicula alumno = db.Peliculas.Find(id);
+                        db.Entry(alumno).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static List<Pelicula> List()
+        {
+            Entities db = new Entities(); //Instancia del contexto
+            
+            return db.Peliculas.ToList();
+        }
+
+    }
+}
---
+++ SlnTaller1/PryTaller1/Controllers/PeliculasController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BEEUTaller1;
+using BEEUTaller1.Transactions;
+
+namespace PryTaller1.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            return View(PeliculaBLL.List());
+        }
+
+        // GET: Peliculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idpelicula,nombre,fecha_lanzamiento,categoria,duracion")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                PeliculaBLL.Create(pelicula);
+                return RedirectToAction("Index");
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idpelicula,nombre,fecha_lanzamiento,categoria,duracion")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                //LINEAS EDITADAS 
+                PeliculaBLL.Update(pelicula);
+                return RedirectToAction("Index");
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pelicula pelicula = db.Peliculas.Find(id);
+            db.Peliculas.Remove(pelicula);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
